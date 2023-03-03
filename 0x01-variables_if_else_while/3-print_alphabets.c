ce9aeeb9b7e8e21704126f172fc36ae318ebca59
budda <jackbudda@gmail.com>
2023-03-03 09:58:44 -0800

    1-last_digit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stadio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	for ( i = 0; i < 52; itt)
+	{
+		putcha(alp[1]);
+	}
+	putchar('\n');
+	return (0);
+}
